c5357bc8e00a37e88fda860277d61e890ffc92df
caesar <sesar091913@gmail.com>
2025-07-20 20:34:34 +0800

    added error handling

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,69 +1,68 @@
 import Product from "../model/product.js";
+import catchAsyncError from "../middleware/catchAsyncError.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 // create product => api/v1/product
-export const createProduct = async (req, res) => {
-    const product = await Product.create(req.body);
+export const createProduct = catchAsyncError(async (req, res) => {
+  const product = await Product.create(req.body);
 
-    res.status(200).json({
-        product
-    })
-}
+  res.status(200).json({
+    product,
+  });
+});
 
 // get Product => api/v1/product
-export const getProducts = async (req, res) => {
-    const product = await Product.find();
+export const getProducts = catchAsyncError(async (req, res) => {
+  const product = await Product.find();
+  let productsLength = product.length
 
-    res.status(200).json({
-        product
-    })
-}
+  res.status(200).json({
+    productsLength,
+    product,
+  });
+});
 
 // get specific product => api/v1/product/:id
-export const getProductDetails = async (req, res) => {
-    const product = await Product.findById(req?.params?.id)
+export const getProductDetails = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req?.params?.id);
 
-    if(!product){
-        res.status(404).json({
-            err: "Product not found"
-        })
-    }
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
 
-    res.status(200).json({
-        product,
-    })
-}
+  res.status(200).json({
+    product,
+  });
+});
 
 // update product = api/v1/admin/product/:id
-export const updateProduct = async (req, res) => {
-    let product = await Product.findById(req?.params?.id);
+export const updateProduct = catchAsyncError(async (req, res, next) => {
+  let product = await Product.findById(req?.params?.id);
 
-    if(!product){
-        res.status(404).json({
-            error: "Product not found"
-        })
-    }
+  if (!product) {
+    return next(new ErrorHandler("Error updating this product", 404));
+  }
 
-    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {new: true});
+  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+    new: true,
+  });
 
-    res.status(200).json({
-        product,
-    })
-}
+  res.status(200).json({
+    product,
+  });
+});
 
 // delete product = api/v1/product/:id
-export const deleteProduct = async (req, res) => {
-    const product = await Product.findById(req?.params?.id)
+export const deleteProduct = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req?.params?.id);
 
-    if(!product){
-        res.status(404).json({
-            error: "Product not found"
-        })
-    }
+  if (!product) {
+    return next(new ErrorHandler("Error deleting this product", 404));
+  }
 
-    await product.deteleOne();
+  await product.deteleOne();
 
-    res.status(200).json({
-        message: "Product deleted"
-    })
-}
-
+  res.status(200).json({
+    message: "Product deleted",
+  });
+});
--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -1,12 +1,24 @@
 // middleware/errors.js
 export default (err, req, res, next) => {
-    let error = {
-        statusCode: err.statusCode || 500,
-        message: err.message || "Internal Server Error"
-    };
+  let error = {
+    statusCode: err.statusCode || 500,
+    message: err.message || "Internal Server Error",
+  };
 
+  //error message in development mode
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
     res.status(error.statusCode).json({
-        success: false,
-        message: error.message
+      error: err,
+      message: error.message,
+      stack: err?.stack,
     });
+  }
+
+  //error message in production mode
+  if(process.env.NODE_ENV === "PRODUCTION"){
+    res.status(error.statusCode).json({
+        message: error.message,
+    })
+  }
+
 };
